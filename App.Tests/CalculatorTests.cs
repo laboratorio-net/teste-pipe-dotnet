c6e750c8c7fe6865e122deef110c23d38a182318
Leandro Alves <leandro.silva.alves86@gmail.com>
2026-04-09 19:51:18 -0300

    refactor: Simplify CalculatorTests by reusing Calculator instance

--- App.Tests/CalculatorTests.cs
+++ App.Tests/CalculatorTests.cs
@@ -2,48 +2,40 @@
 
 public class CalculatorTests
 {
+    private readonly Calculator _calc = new ();
+
     [Fact(DisplayName = "Deve somar dois numeros")]
     public void ShouldSumTwoNumbers()
-    {
-        var cal = new Calculator();
-        cal.Sum(1, 2);
-
-        Assert.Equal(3, cal.Result);
+    {        
+        _calc.Sum(1, 2);
+        Assert.Equal(3, _calc.Result);
     }
 
     [Fact(DisplayName = "Zero + Zero deve ser zero")]
     public void ZeroPlusZeroShouldBeZero()
-    {
-        var cal = new Calculator();
-        cal.Sum(0, 0);
-
-        Assert.Equal(0, cal.Result);
+    {        
+        _calc.Sum(0, 0);
+        Assert.Equal(0, _calc.Result);
     }
 
     [Fact(DisplayName = "Deve multiplicar multiplos números")]
     public void ShouldSumMultiplesNumbers()
-    {
-        var cal = new Calculator();
-        cal.Sum(1, 2, 3, 4, 5);
-
-        Assert.Equal(15, cal.Result);
+    {        
+        _calc.Sum(1, 2, 3, 4, 5);
+        Assert.Equal(15, _calc.Result);
     }
 
     [Fact(DisplayName = "Deve subtrair dois números")]
     public void ShouldSubtractTwoNumbers()
-    {
-        var cal = new Calculator();
-        cal.Subtract(2, 1);
-
-        Assert.Equal(1, cal.Result);
+    {        
+        _calc.Subtract(2, 1);
+        Assert.Equal(1, _calc.Result);
     }
 
     [Fact(DisplayName = "Subracao deve ter resultado zero quando dois numeros iguais")]
     public void ResultShouldBeZeroWhenSubtractTheSameNumers()
-    {
-        var cal = new Calculator();
-        cal.Subtract(2, 2);
-
-        Assert.Equal(0, cal.Result);
+    {        
+        _calc.Subtract(2, 2);
+        Assert.Equal(0, _calc.Result);
     }
 }
